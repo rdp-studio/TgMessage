e9e9a466c747324fb9f52bbd8410490eac87185f
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-07-12 03:52:40 +0800

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -9,4 +9,8 @@ if ($message === '/token') {
     $chat_id = $data['message']['chat']['id'];
     $bot->sendMessage(['text' => $bot->encryption($chat_id), 'chat_id' => $chat_id]);
 }
+if ($message === '/start') {
+    $chat_id = $data['message']['chat']['id'];
+    $bot->sendMessage(['text' => $bot->encryption($chat_id), 'chat_id' => $chat_id]);
+}
 echo json_encode(['code' => 200, 'message' => 'success']);
